c2e4efa5c3ef6704d572679d5c0bb462f6b259bc
mbgoseco <mbgoseco@gmail.com>
2019-02-04 12:05:49 -0800

    last of controllers wireframe

--- CookBookAPI/CookBookAPI/Data/CookBookDBContext.cs
+++ CookBookAPI/CookBookAPI/Data/CookBookDBContext.cs
@@ -1,16 +1,23 @@
 ﻿using CookBookAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CookBookAPI.Data
 {
-    public class CookBookDBContext : DbContext
+    public class CookBookDbContext : DbContext
     {
-        public CookBookDBContext(DbContextOptions<CookBookDBContext> options) : base(options)
+        public CookBookDbContext(DbContextOptions<CookBookDbContext> options) : base (options)
         {
 
 
         }
 
         DbSet<RecipieIngredients> RecipieIngredients { get; set; }
+        DbSet<Ingredients> Ingredients { get; set; }
+        DbSet<Recipes> Recipes { get; set; }
+        DbSet<Instructions> Instructions { get; set; }
     }
 }
